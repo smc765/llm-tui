3d16a41a51acabfe854a89b07ac90ee539e3593e
Sam Crouch <smc3nc@umsystem.edu>
2026-05-10 17:41:17 -0500

    changes

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from dataclasses import dataclass
 
 import llm
 from dotenv import load_dotenv
+import pyperclip
 
 from textual import on, work
 from textual.app import App, ComposeResult
@@ -48,33 +49,32 @@ class Prompt(Markdown):
 class Response(Markdown):
     """Markdown for llm response."""
 
-    @dataclass
-    class Regenerate(Message):
-        prompt: str
-        attachments: list[llm.Attachment]
-
-    def __init__(self, prompt: str, attachments: list[llm.Attachment], model_id: str):
+    def __init__(self, prompt: str, attachments: list[llm.Attachment], model_id: str, pandoc_path: str | None):
         super().__init__()
         self.prompt = prompt
         self.attachments = attachments
         self.model_id = model_id
-        self.border_subtitle = f"Attachments: {len(attachments)}"
         self.worker: Worker | None = None
+        self.pandoc_path = pandoc_path
 
     def compose(self) -> ComposeResult:
         yield HorizontalGroup(
             Button("Regenerate", id="regenerate"),
             Button("Open in Browser", id="open_in_browser"),
             Button("Cancel", id="cancel"),
+            Button("Copy to Clipboard", id="copy"),
+            # Button("Save As", id="save_as"), # TODO
         )
 
     def on_mount(self) -> None:
         self.border_title = self.model_id
         self.update_subtitle()
+        # if self.pandoc_path is None:
+        #     self.query_one("#open_in_brownser").remove()
 
     @on(Button.Pressed, "#regenerate")
-    def regenerate(self) -> None:
-        self.post_message(self.Regenerate(self.prompt, self.attachments))
+    async def regenerate(self) -> None:
+        await self.app.get_response(self.prompt, self.attachments)
 
     @on(Button.Pressed, "#cancel")
     def cancel(self) -> None:
@@ -83,14 +83,18 @@ class Response(Markdown):
 
     @on(Button.Pressed, "#open_in_browser")
     def open_in_browser(self) -> None:
-        # text = re.sub(r"\\\[(.*?)\\\]", r"$$\1$$", self.source, flags=re.DOTALL)
-        text = self.source
+        if self.pandoc_path is None:
+            self.app.get_vertical_scroll().mount(Prompt("Install [Pandoc](https://pandoc.org/installing.html) to use this feature or set the PANDOC_PATH environment variable if already installed."))
+            return
+
+        # text = re.sub(r"\\\[(.*?)\\\]", r"$$\1$$", self.source, flags=re.DOTALL) # failed attempt to convert delimiters
+
         with tempfile.NamedTemporaryFile(delete_on_close=False, suffix=".md", mode="w", encoding="utf-8") as temp_md:
-            temp_md.write(text)
+            temp_md.write(self.source)
             temp_md.close()
             cmd = [
-                "pandoc", temp_md.name,
-                "-s", "--mathjax",
+                self.pandoc_path, temp_md.name,
+                # "-s", "--mathjax",
                 "-o", "out.html",
             ]
             try:
@@ -100,7 +104,36 @@ class Response(Markdown):
                 logger.error(e)
                 return
 
+#         with open("out.html","w") as f:
+#             f.write(f'''
+# <!DOCTYPE html>
+# <html>
+#     <head>
+#         <title>{self.model_id}</title>
+#         <style>
+#             :root {{color-scheme: dark;
+#             font-family: Arial, Helvetica, sans-serif;}}
+#         </style>
+#     </head>
+#     <body>
+#         <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+#         <div>
+#         {self.source} # TODO this works but should use pandoc to render markdown
+#         </div>
+#     </body>
+# </html>'''
+#             )
+
+        if os.path.isfile("out.html"):
             webbrowser.open("out.html")
+
+    @on(Button.Pressed, "#copy")
+    def copy(self) -> None:
+        pyperclip.copy(self.source)
+
+    @on(Button.Pressed, "#save_as")
+    def save_as(self) -> None:
+        raise NotImplementedError
 
     def update_subtitle(self, input_tokens: int = 0, output_tokens: int = 0) -> None:
         if input_tokens and output_tokens:
@@ -109,19 +142,27 @@ class Response(Markdown):
         else:
             self.border_subtitle = f"Attachments: {len(self.attachments)}"
 
-    def finalize(self) -> None:
-        self.query_one("#cancel").remove()
-
 
 class PromptInput(Input):
 
-    def _on_paste(self, event: Paste) -> None:
-        event.stop()
-        if self.value or not event.text:
-            return
+    @dataclass
+    class FixPaste(Message):
+        input: Input
+        first_line: str
 
-        if len(event.text.splitlines()) > 1:
-            self.post_message(self.Submitted(self, event.text, None))
+    def _on_paste(self, event: Paste) -> None:
+        if event.text:
+            first_line = event.text.splitlines()[0]
+            text = re.sub(r"\r?\n", " ", event.text)
+            selection = self.selection
+            if selection.is_empty:
+                self.insert_text_at_cursor(text)
+            else:
+                self.replace(text, *selection)
+                
+            self.post_message(self.FixPaste(self, first_line))
+
+        event.stop()
 
 
 class TuiApp(App):
@@ -136,23 +177,26 @@ class TuiApp(App):
         ("f3", "attach_file", "Attach File(s)"),
         ("f4", "attach_screenshot", "Screenshot"),
         ("f5", "clear_context", "Clear Context"),
-        ("f6", "multiline_prompt", "Multiline Prompt"),
+        ("f6", "edit_prompt", "Prompt Editor"),
         ("f7", "clear_attachments", "Clear Attachments"),
+        ("f8", "edit_model_options", "Model Options"),
     ]
 
     def __init__(self, temp_dir: str):
         super().__init__()
         self.temp_dir = temp_dir
         self.model = llm.get_model(os.getenv("DEFAULT_MODEL", DEFAULT_MODEL))
-        self.conversation = self.model.conversation()
         self.system_prompt = os.getenv("DEFAULT_SYSTEM_PROMPT")
         self.attachments: list[llm.Attachment] = []
-        self.model_options: dict[str, Any] = {}
-        self.input_tokens = 0
-        self.output_tokens = 0
 
-        if model_options := os.getenv("MODEL_OPTIONS"):
-            self.model_options = ast.literal_eval(model_options)
+        self.parse_model_options(os.getenv("MODEL_OPTIONS"))
+
+        self.pandoc_path = None
+        if (path := os.getenv("PANDOC_PATH")) and os.path.isfile(path):
+            self.pandoc_path = path
+
+        elif "pandoc" in os.getenv("PATH", "").lower():
+            self.pandoc_path = "pandoc"
 
     def compose(self) -> ComposeResult:
         yield VerticalScroll()
@@ -162,21 +206,22 @@ class TuiApp(App):
 
     def on_mount(self) -> None:
         self.get_vertical_scroll()
+        self.new_conversation()
 
     @on(Input.Submitted)
     async def on_input(self, event: Input.Submitted) -> None:
         event.input.clear()
         await self.send_prompt(event.value)
 
-    @on(Response.Regenerate)
-    async def regenerate(self, event: Response.Regenerate) -> None:
-        await self.get_response(event.prompt, event.attachments)
+    @on(PromptInput.FixPaste)
+    def fix_paste(self, event: PromptInput.FixPaste) -> None:
+        event.input.value = event.input.value.removesuffix(event.first_line)
 
     def action_set_model(self) -> None:
         def set_model(model: str) -> None:
             if model != self.model.model_id:
                 self.model = llm.get_model(model)
-                self.clear_context()
+                self.new_conversation()
                 
             self.notify(f"Model set to: {model}")
 
@@ -195,7 +240,7 @@ class TuiApp(App):
         self.push_screen(TextEditor(self.system_prompt), set_system_prompt)
 
     def action_clear_context(self) -> None:
-        self.clear_context()
+        self.new_conversation()
         self.notify("Context cleared")
 
     def action_attach_file(self) -> None:
@@ -217,12 +262,15 @@ class TuiApp(App):
 
             self.attach_file(temp.name)
 
-    def action_multiline_prompt(self) -> None:
-        self.push_screen(TextEditor(), self.send_prompt)
+    def action_edit_prompt(self) -> None:
+        self.push_screen(TextEditor(self.query_one(Input).value, update_input=True), self.send_prompt)
 
     def action_clear_attachments(self) -> None:
         self.clear_attachments()
         self.notify("attachments cleared")
+
+    def action_edit_model_options(self) -> None:
+        self.push_screen(TextEditor(str(self.model_options)), self.parse_model_options)
 
     def check_action(self, action: str, parameters: tuple[object, ...]) -> bool:  
         if action == "clear_attachments":
@@ -239,6 +287,17 @@ class TuiApp(App):
 
         return True
 
+    def parse_model_options(self, text: str | None):
+        self.model_options = {}
+        if not text:
+            return
+
+        try:
+            self.model_options = ast.literal_eval(text)
+        except Exception as e:
+            logger.error(e)
+            self.notify("Could not parse model options. Check syntax.", title="Error")
+
     def attach_file(self, filename: str) -> None:
         mime, _ = mimetypes.guess_type(filename)
         if mime not in self.model.attachment_types:
@@ -252,10 +311,9 @@ class TuiApp(App):
         self.attachments.clear()
         self.update_gui()
 
-    def clear_context(self) -> None:
+    def new_conversation(self) -> None:
         self.conversation = self.model.conversation()
-        self.input_tokens = 0
-        self.output_tokens = 0
+        self.input_tokens = self.output_tokens = 0
         self.update_gui()
     
     def update_gui(self) -> None:
@@ -279,7 +337,7 @@ class TuiApp(App):
         await self.get_response(prompt, attachments)
 
     async def get_response(self, prompt: str, attachments: list[llm.Attachment]) -> None:
-        response = Response(prompt, attachments, self.model.model_id)
+        response = Response(prompt, attachments, self.model.model_id, self.pandoc_path)
         await self.get_vertical_scroll().mount(response)
         model_options = self.get_supported_options(self.model, self.model_options)
         response.worker = self.stream_response(response, model_options)
@@ -327,7 +385,7 @@ class TuiApp(App):
             logger.error(e)
 
         finally:
-            self.call_from_thread(response.finalize)
+            response.query_one("#cancel").remove()
 
             if input_tokens and output_tokens:
                 if self.input_tokens == -1:
@@ -370,15 +428,16 @@ class TextEditor(ModalScreen):
     AUTO_FOCUS = "TextArea"
     BINDINGS = [
         ("ctrl+c", "app.quit"),
-        ("escape", "app.pop_screen", "Back"),
+        ("escape", "back", "Back"),
         ("f1", "submit", "Submit"),
         ("f2", "clear", "Clear"),
         ("f3", "load_file", "Load File"),
     ]
 
-    def __init__(self, text: str | None = None):
+    def __init__(self, text: str | None = None, update_input: bool = False):
         super().__init__()
         self.text = text if text else ""
+        self.update_input = update_input
 
     def compose(self) -> ComposeResult:
         yield TextArea(self.text)
@@ -387,6 +446,13 @@ class TextEditor(ModalScreen):
     def action_submit(self) -> None:
         text = self.query_one(TextArea).text
         self.dismiss(text)
+
+    def action_back(self) -> None:
+        if self.update_input:
+            text = re.sub(r"\r?\n", " ", self.query_one(TextArea).text)
+            self.app.query_one(PromptInput).value = text
+
+        self.app.pop_screen()
 
     def action_clear(self) -> None:
         self.query_one(TextArea).clear()
